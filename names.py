c4dcaed1b2bbeb16ae0c2227619adbad1cf3a2aa
Shrinikesh Arulselvan <sa787@cam.ac.uk>
2018-05-22 12:16:37 +0100

    Implemented Names module as per specification along with implementing corresponding Unittesting file test_names.py - at current commit unit tests all pass

--- names.py
+++ names.py
@@ -41,12 +41,15 @@ class Names:
     def __init__(self):
         """Initialise names list."""
         self.error_code_count = 0  # how many error codes have been declared
+        self.names=[]# initialising the names_list
 
     def unique_error_codes(self, num_error_codes):
         """Return a list of unique integer error codes."""
         if not isinstance(num_error_codes, int):
             raise TypeError("Expected num_error_codes to be an integer.")
         self.error_code_count += num_error_codes
+
+
         return range(self.error_code_count - num_error_codes,
                      self.error_code_count)
 
@@ -55,15 +58,53 @@ class Names:
 
         If the name string is not present in the names list, return None.
         """
+        if not isinstance(name_string, str):
+            raise TypeError('name_string must be a string')
+        elif name_string in self.names:
+            return self.names.index(name_string)
+        else:
+            return None
+
+
 
     def lookup(self, name_string_list):
         """Return a list of name IDs for each name string in name_string_list.
 
         If the name string is not present in the names list, add it.
         """
+        # checkingif name_string_list is a list:
+        if not isinstance(name_string_list, list):
+            raise TypeError('name_string_list must be a list')
+        #instantiating return list:
+        name_id_list = []
+        for name_string in name_string_list:
+            #checking if elements of list are actually strings
+            if not isinstance(name_string, str):
+                raise TypeError('name_string_list must be composed of only strings')
+            if name_string in self.names:
+                name_id_list.append(self.names.index(name_string))
+            else:
+                #now checking for accidental empty strings being put into name_string_list
+                if len(name_string) > 0:
+                    self.names.append(name_string)
+                    name_id_list.append(self.names.index(name_string))
+        return name_id_list
+
+
 
     def get_name_string(self, name_id):
         """Return the corresponding name string for name_id.
 
         If the name_id is not an index in the names list, return None.
         """
+        # checking if name_id is an integer
+        if not isinstance(name_id, int):
+            raise TypeError("name_id must be a non-negative integer")
+        elif name_id < 0:
+            raise IndexError("name_id must be non-negative")
+            # since we don't want to pick up the last element if someone types
+            # "-1" for example
+        elif name_id < len(self.names):
+            return self.names[name_id]
+        else:
+            return None
